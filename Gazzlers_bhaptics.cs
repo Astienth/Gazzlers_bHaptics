196ee07274926e0874be92a5c95444f5bbd1b859
Astien <astienth@gmail.com>
2023-09-20 22:14:47 +0200

    initial commit

---
+++ Gazzlers_bhaptics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+using System.Collections.Generic;
+
+namespace Gazzlers_bhaptics
+{
+    [BepInPlugin("org.bepinex.plugins.Gazzlers_bhaptics", "Gazzlers_bhaptics integration", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+#pragma warning disable CS0109 // Remove unnecessary warning
+        internal static new ManualLogSource Log;
+#pragma warning restore CS0109
+        public static TactsuitVR tactsuitVr;
+
+        private void Awake()
+        {
+            // Make my own logger so it can be accessed from the Tactsuit class
+            Log = base.Logger;
+            // Plugin startup logic
+            Logger.LogMessage("Plugin Gazzlers_bhaptics is loaded!");
+            tactsuitVr = new TactsuitVR();
+            // one startup heartbeat so you know the vest works correctly
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+            // patch all functions
+            var harmony = new Harmony("bhaptics.patch.Gazzlers_bhaptics");
+            harmony.PatchAll();
+        }
+    }
+    /*
+    [HarmonyPatch(typeof(EntityPlayerLocal), "LateUpdate")]
+    public class bhaptics_OnUpdate
+    {
+        [HarmonyPostfix]
+        public static void Postfix(EntityPlayerLocal __instance)
+        {
+            Plugin.currentHealth = Traverse.Create(__instance).Field("oldHealth").GetValue<float>();
+        }
+    }
+    */
+}
+
---
+++ MyBhapticsTactsuit.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Bhaptics.Tact;
+using Gazzlers_bhaptics;
+using UnityEngine;
+
+namespace MyBhapticsTactsuit
+{
+
+    public class TactsuitVR
+    {
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // Event to start and stop the heartbeat thread
+        private static ManualResetEvent HeartBeat_mrse = new ManualResetEvent(false);
+        public static bool headUnderwater = false;
+        // dictionary of all feedback patterns found in the bHaptics directory
+        public Dictionary<String, FileInfo> FeedbackMap = new Dictionary<String, FileInfo>();
+        private static RotationOption defaultRotationOption = new RotationOption(0.0f, 0.0f);
+
+#pragma warning disable CS0618 // remove warning that the C# library is deprecated
+        public HapticPlayer hapticPlayer;
+#pragma warning restore CS0618 
+
+        public void HeartBeatFunc()
+        {
+            while (true)
+            {
+                // Check if reset event is active
+                HeartBeat_mrse.WaitOne();
+                PlaybackHaptics("HeartBeat");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public TactsuitVR()
+        {
+
+            LOG("Initializing suit");
+            try
+            {
+#pragma warning disable CS0618 // remove warning that the C# library is deprecated
+                hapticPlayer = new HapticPlayer("Gazzlers_bhaptics", "Gazzlers_bhaptics");
+#pragma warning restore CS0618
+                suitDisabled = false;
+            }
+            catch { LOG("Suit initialization failed!"); }
+            RegisterAllTactFiles();
+            LOG("Starting HeartBeat thread...");
+            Thread HeartBeatThread = new Thread(HeartBeatFunc);
+            HeartBeatThread.Start();
+        }
+
+        public void LOG(string logStr)
+        {
+            Plugin.Log.LogMessage(logStr);
+        }
+
+
+        void RegisterAllTactFiles()
+        {
+            if (suitDisabled) { return; }
+            // Get location of the compiled assembly and search through "bHaptics" directory and contained patterns
+            string assemblyFile = Assembly.GetExecutingAssembly().Location;
+            string myPath = Path.GetDirectoryName(assemblyFile);
+            LOG("Assembly path: " + myPath);
+            string configPath = myPath + "\\bHaptics";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.tact", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    hapticPlayer.RegisterTactFileStr(prefix, tactFileStr);
+                    LOG("Pattern registered: " + prefix);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+                FeedbackMap.Add(prefix, Files[i]);
+            }
+            systemInitialized = true;
+        }
+
+        public void PlaybackHaptics(String key, bool forced = true, float intensity = 1.0f, float duration = 1.0f)
+        {
+            if (suitDisabled) { return; }
+            if (FeedbackMap.ContainsKey(key))
+            {
+                ScaleOption scaleOption = new ScaleOption(intensity, duration);
+                if (hapticPlayer.IsPlaying() && !forced)
+                {
+                    return;
+                }
+                else
+                {
+                    hapticPlayer.SubmitRegisteredVestRotation(key, key, defaultRotationOption, scaleOption);
+                }
+            }
+            else
+            {
+                LOG("Feedback not registered: " + key);
+            }
+        }
+
+        public void StartHeartBeat()
+        {
+            HeartBeat_mrse.Set();
+        }
+
+        public void StopHeartBeat()
+        {
+            HeartBeat_mrse.Reset();
+        }
+
+        public void StopHapticFeedback(String effect)
+        {
+            hapticPlayer.TurnOff(effect);
+        }
+
+        public void StopAllHapticFeedback()
+        {
+            StopThreads();
+            foreach (String key in FeedbackMap.Keys)
+            {
+                hapticPlayer.TurnOff(key);
+            }
+        }
+
+        public void StopThreads()
+        {
+            StopHeartBeat();
+        }
+
+
+    }
+}
